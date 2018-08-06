71227b170dd27ac2622907321c4563e8727ee057
davidseo <jseo49ut@gmail.com>
2018-08-06 18:30:15 -0400

    converting temperature into Celsius and outputing the result

--- exercise5.rb
+++ exercise5.rb
@@ -7,3 +7,7 @@ temperature = gets.chomp
 def convert(fahrenheit)
   C = (fahrenheit.to_i - 32) * 5 / 9
 end
+
+converted_temp = convert(temperature)
+
+puts "#{temperature}F is #{converted_temp} in Celsius."
