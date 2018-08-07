7c79834f92546f3dd6cbb26a1e14fc76f8d27d64
davidseo <jseo49ut@gmail.com>
2018-08-07 11:17:02 -0400

    Defining method to display cohort names and their corresponding number of students

---
+++ exercise7.rb
@@ -0,0 +1,15 @@
+# exercise 7 !
+
+students = {
+  :cohort1 => 34,
+  :cohort2 => 42,
+  :cohort3 => 22
+}
+
+def display(hash)
+  hash.each do |key, value|
+    puts "#{key}: #{value} students"
+  end
+end
+
+display(students)
