f6395c92b7518f303067836c64714b0e4032a754
davidseo <jseo49ut@gmail.com>
2018-08-06 20:31:19 -0400

    Count # of items and output the total number

--- exercise6.rb
+++ exercise6.rb
@@ -2,15 +2,18 @@
 
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
+# method to output each grocery item with an asterisk in front of each
 def write_grocery_list(array)
   array.each do |items|
     puts "* #{items}"
   end
 end
 
+# add rice to the grocery list
 grocery_list.push("rice")
 
 write_grocery_list(grocery_list)
 
-
+# count and output number of items
+puts "The total number of grocery items is #{grocery_list.count}."
 
