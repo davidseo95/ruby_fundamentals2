93d69d199046027c06616fb175f6b75abf1446ed
davidseo <jseo49ut@gmail.com>
2018-08-06 20:23:30 -0400

    added rice to the grocery list and defined method that outputs each item of the grocery list

--- exercise6.rb
+++ exercise6.rb
@@ -2,6 +2,15 @@
 
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
-grocery_list.each do |items|
-  puts "* #{items}"
+def write_grocery_list(array)
+  array.each do |items|
+    puts "* #{items}"
+  end
 end
+
+grocery_list.push("rice")
+
+write_grocery_list(grocery_list)
+
+
+
